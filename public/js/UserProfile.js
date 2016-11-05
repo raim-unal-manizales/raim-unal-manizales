cf12fa384a728151c3e73cc05455c7553a3e306f
magiraldooc <magiraldooc@gmail.com>
2016-11-05 12:46:36 -0500

    Implementación de reglas de adaptación de need

--- public/js/UserProfile.js
+++ public/js/UserProfile.js
@@ -3,13 +3,44 @@
  */
 
 function UserProfile(){
-    var etnica;
-    
-    var lenguaSenas;
-    
-    var nivelAuditivo;
 
-    var nivelVisual;
-    
-    var estiloAprendizaje;
+    var idioma;
+    var nivel_escolaridad;
+
+    //Estilo de aprendizaje
+    var estilo_aprendizaje;
+    var ls_vark;
+    var ls_dicotomia;
+    var ls_visual;
+    var ls_kinestesico;
+    var ls_auditivo;
+    var ls_lector;
+    var ls_global;
+    var ls_secuencial;
+
+    //Need
+    var need;       //Presenta alguna need
+
+    var need_visual;
+    var need_v1;    //Visión nula
+    var need_v2;    //Vision baja
+
+    var need_auditiva;
+    var need_a1;    //
+    var need_a2;    //
+    var need_a3;    //
+
+    var need_motriz;
+    var need_m1;    //Manipula teclado
+    var need_m2;    //Manipula mouse
+    var need_m3;    //Manipula tecnologías asistivas
+
+    var need_cognitiva;
+    var need_c1;    //Dificultad comprender textos
+    var need_c2;    //Dificultad seguir instrucciones
+    var need_c3;    //Dificultad concentración
+
+    var need_etnica;
+    var need_e1;    //Étnia a la que pertenece
+
 }
