812fa4d0d0ab49904e8904af24831963054e3e07
daespinosag <daespinosag@unal.edu.co>
2017-01-02 21:23:48 -0500

    refactoring: include PublicController

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -6,24 +6,13 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use App\Entities\FieldUser;
-use App\Entities\Aplication;
-use App\Entities\Table;
-use App\Entities\TypeField;
-use App\Entities\FieldTable;
-use App\Entities\Option;
-use App\Entities\User;
-use App\Entities\Rol;
-
 use App\Repositories\AplicationRepository;
 
 class PublicController extends Controller
 {
   public $aplicationRepository;
 
-  public function __construct(
-    AplicationRepository $aplicationRepository
-  )
+  public function __construct(AplicationRepository $aplicationRepository)
   {
     $this->aplicationRepository = $aplicationRepository;
   }
@@ -46,73 +35,11 @@ class PublicController extends Controller
     public function searchOa($id = null)
     {
         if($id != null){
-            $aplications = Aplication::where('rquiered_information','True')->get();
-            $aplications -> user_id = $id;
-
-            foreach ($aplications as $key => $value) {
-                $value -> user_id = $id;
-            }
-            $aplications->each(function ($aplications)
-            {
-                $tables = Table::where('id_app',$aplications->id)->get();
-                foreach ($tables as $key => $value) {
-                    $value -> user_id = $aplications->user_id;
-                }
-
-                $tables->each(function ($tables)
-                {
-                    $fieldTables = FieldTable::where('id_table',$tables->id)->get();
-                    foreach ($fieldTables as $key => $value) {
-                        $value -> user_id = $tables->user_id;
-                    }
-
-                    $fieldTables->each(function ($fieldTables)
-                    {
-                        $id_fiel_tables = $fieldTables->id;
-                        $fielduser = FieldUser::where('id_user',$fieldTables->user_id)->where('id_field_table',$id_fiel_tables)->get()->first();
-
-                        $types_fields= TypeField::where('id',$fieldTables->id_type_field)->get();
-                        $fieldTables-> types_fields = $types_fields;
-
-                        $options= Option::where('id_field_table',$id_fiel_tables)->lists('name', 'id');
-                        $fieldTables-> options = $options;
-
-                        if (is_null($fielduser)) {
-
-                            $fieldTables-> value = null;
-                            $fieldTables-> id_defect = null;
-                        }else{
-
-                            $types_fields_select= TypeField::select('html')->where('id',$fieldTables->id_type_field)->lists('html')->all();
-
-                            if ($types_fields_select[0] == "select") {
-
-                                $option= Option::select('name')->where('id',$fielduser->value)->lists('name')->all();
-
-                                $fieldTables-> value = $option[0];
-
-                            }else{
-
-                                $fieldTables-> value = $fielduser->value;
-                            }
-
-                            $fieldTables-> id_defect = $fielduser->id;
-                        };
-                    });
-                    $tables-> fields_tables = $fieldTables->all();
-                });
-                $aplications -> tablas = $tables;
-            });
-            #--------------------------------------------------------------------------------------
-
-            $fielduser = FieldUser::find($id);
-
-            return view('public.searchOa')
+          $aplications =  $this->Consult_Aplications($id);
+          return view('public.searchOa')
                 ->with('aplications', $aplications)
-                ->with('user_id',$id)
-                ->with('fielduser',$fielduser);
+                ->with('user_id',$id);
         }
-
         return view('public.searchOa')
             ->with('aplications', null);
 
