f586e8b9156dabadc73e1505061d01e0c94e62b0
daespinosag <daespinosag@unal.edu.co>
2017-01-20 12:35:24 -0500

    assent  in aplication

--- resources/views/public/aplications.blade.php
+++ resources/views/public/aplications.blade.php
@@ -11,30 +11,30 @@
 <div class="row">
 	@foreach($aplications as $aplication)
     <div class="col-lg-3 col-md-6">
-		
+
 			<div class="col.md-2">
 				<div class="panel panel-default">
 					<div class="panel-body text-center">
-								
+
 						<a href="{{ asset(''.$aplication->url) }} " target="_blank" class="">
-							<img class="img-responsive img-article"  style="max-width: 100px; max-height: 100px; min-width: 100px; min-height: 100px;"  src="{{ $aplication->logo }}" alt="...">
+							<img class="img-responsive img-article"  style="max-width: 100px; max-height: 100px; min-width: 100px; min-height: 100px;"  src="{{ asset($aplication->logo) }}" alt="...">
 						</a>
-								
+
 						<a href="" class=""><h3 class="text-center"></h3></a>
 						<hr>
-						
+
 						<i class="fa fa-folder-open-o"></i>
 
 						 <a href="{{ asset(''.$aplication->url) }}" target="_blank">{{ $aplication->name }}</a>
-								
+
 						<div class="pull-right">
 							<i class="glyphicon glyphicon-share-alt"></i>
 						</div>
-								
-					</div>						
-				</div>					
+
+					</div>
+				</div>
 			</div>
-		
+
    	</div>
    	@endforeach
 </div>
